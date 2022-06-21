1723d8c72335e4b0e3fb816e407cda9d9c34bc13
nguyen13901@gmail.com <nguyen13901@gmail.com>
2022-06-21 14:03:26 +0700

    Implement search product 21.06.2022

--- api_product/urls.py
+++ api_product/urls.py
@@ -4,16 +4,9 @@ from api_product import views
 
 
 urlpatterns = [
-<<<<<<< HEAD
-    path("", include(router.urls)),
-    path('products/<slug:category_slug>/<slug:product_slug>/', views.DetailProductView.as_view(),
-         name="detail_product"),
-    path('products/<slug:category_slug>', views.CategoryProductView.as_view(), name="product_by_category"),
-=======
     path('products/', views.LatestProductsListView.as_view()),
     path('products/search/', views.search),
     path('products/<slug:category_slug>/<slug:product_slug>/', views.DetailProductView.as_view(),
          name="detail_product"),
     path('products/<slug:category_slug>/', views.CategoryProductView.as_view(), name="product_by_category"),
->>>>>>> Implement category view 17.06.2022
 ]

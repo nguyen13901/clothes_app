f09707447bc1dfe9eb8e061f7269d2cdc3f69102
nguyen13901@gmail.com <nguyen13901@gmail.com>
2022-06-23 10:18:23 +0700

    Implement checkout feature 23.06.2022

--- frontend/src/views/CheckOut.vue
+++ frontend/src/views/CheckOut.vue
@@ -1,48 +1,119 @@
 <template>
-  <div class="page-checkout">
-    <div class="columns is-multiline">
-      <div class="column is-12">
-        <h1 class="title">CheckOut</h1>
-      </div>
+    <div class="page-checkout">
+        <div class="columns is-multiline">
+            <div class="column is-12">
+                <h1 class="title">Checkout</h1>
+            </div>
 
-      <div class="column is-12 box">
-        <table class="table is-fullwidth">
-          <thead>
-            <tr>
-              <th>Product</th>
-              <th>Price</th>
-              <th>Quantity</th>
-              <th>Total</th>
-            </tr>
-          </thead>
+            <div class="column is-12 box">
+                <table class="table is-fullwidth">
+                    <thead>
+                        <tr>
+                            <th>Product</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
 
-          <tbody>
-            <tr v-for="item in cart.items" :key="item.id">
-              <td>{{ item.name }}</td>
-              <td>{{ item.price }}</td>
-              <td>{{ item.quantity }}</td>
-              <td>${{ getItemTotal(item).toFixed(2) }}</td>
-            </tr>
-          </tbody>
+                    <tbody>
+                        <tr
+                            v-for="item in cart.items"
+                            v-bind:key="item.product.id"
+                        >
+                            <td>{{ item.product.name }}</td>
+                            <td>${{ item.product.price }}</td>
+                            <td>{{ item.quantity }}</td>
+                            <td>${{ getItemTotal(item).toFixed(2) }}</td>
+                        </tr>
+                    </tbody>
 
-          <tfoot>
-            <tr>
-                <td colspan="2">Total</td>
-                <td>{{ cartTotalLength}}</td>
-                <td>${{ cartTotalLength.toFixed(2) }}</td>
-            </tr>
+                    <tfoot>
+                        <tr>
+                            <td colspan="2">Total</td>
+                            <td>{{ cartTotalLength }}</td>
+                            <td>${{ cartTotalPrice.toFixed(2) }}</td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
 
-          </tfoot>
-        </table>
-      </div>
+            <div class="column is-12 box">
+                <h2 class="subtitle">Shipping details</h2>
 
-      <div class="column is-12 box">
-        <h2 class="subtitle">Shipping details</h2>
+                <p class="has-text-grey mb-4">* All fields are required</p>
 
-        <p class="has-text-grey mb-4">ALl fields are required</p>
-      </div>
+                <div class="columns is-multiline">
+                    <div class="column is-6">
+                        <div class="field">
+                            <label>First name*</label>
+                            <div class="control">
+                                <input type="text" class="input" v-model="first_name">
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <label>Last name*</label>
+                            <div class="control">
+                                <input type="text" class="input" v-model="last_name">
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <label>E-mail*</label>
+                            <div class="control">
+                                <input type="email" class="input" v-model="email">
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <label>Phone*</label>
+                            <div class="control">
+                                <input type="text" class="input" v-model="phone">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="column is-6">
+                        <div class="field">
+                            <label>Address*</label>
+                            <div class="control">
+                                <input type="text" class="input" v-model="address">
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <label>Zip code*</label>
+                            <div class="control">
+                                <input type="text" class="input" v-model="zipcode">
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <label>Place*</label>
+                            <div class="control">
+                                <input type="text" class="input" v-model="place">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="notification is-danger mt-4" v-if="errors.length">
+                    <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
+                </div>
+
+                <hr>
+
+                <div id="card-element" class="mb-5"></div>
+
+                <template v-if="cartTotalLength">
+                    <hr>
+
+                    <button class="button is-dark" @click="submitForm">Pay with Stripe</button>
+                </template>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -74,6 +145,37 @@ export default {
   methods: {
     getItemTotal(item) {
       return item.quantity * item.product.price;
+    },
+    submitForm() {
+      this.errors = []
+
+        if (this.first_name === '') {
+          this.errors.push("The first name field is missing!")
+        }
+
+        if (this.last_name === '') {
+          this.errors.push('The last name field is missing!')
+        } 
+
+        if (this.email === '') {
+          this.errors.push('The email field is missing!')
+        }
+
+        if (this.phone === '') {
+          this.errors.push('The phone field is missing!')
+        }
+
+        if (this.address === '') {
+          this.errors.push('The address field is missing')
+        }
+
+        if (this.zipcode === '') {
+          this.errors.push('The zipcode field is missing!')
+        }
+
+        if (this.place === '') {
+          this.errors.push('The place field is missing!')
+        }
     },
   },
   computed: {

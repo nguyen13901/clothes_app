29ff63ba09cef0ee9c2bd9393ff1076a215844b6
nguyen13901@gmail.com <nguyen13901@gmail.com>
2022-06-24 17:56:48 +0700

    Deploy app on the server 24.06.2022

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,5 +4,6 @@ import router from "./router";
 import store from "./store";
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:8000/'
+// axios.defaults.baseURL = 'http://localhost:8000/'
+axios.defaults.baseURL = 'https://d146-190-2-130-168.eu.ngrok.io'
 createApp(App).use(store).use(router, axios).mount("#app");

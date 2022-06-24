c203447ff77ad87661d1ffa2b4c4da7f877e62d7
nguyen13901@gmail.com <nguyen13901@gmail.com>
2022-06-24 16:35:47 +0700

    Deploy app on the server 24.06.2022

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,5 +5,7 @@ import store from './store'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'https://55cd-190-2-130-168.eu.ngrok.io/'
+axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
 createApp(App).use(store).use(router, axios).mount('#app')

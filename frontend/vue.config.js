3dcad9b41a7f234780ad1098b64734e9276c4bf8
nguyen13901@gmail.com <nguyen13901@gmail.com>
2022-06-24 15:19:23 +0700

    Deploy app on the server 24.06.2022

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   devServer: {
     headers: {
-      "Access-Control-Allow-Origin": "http://localhost:8000/",
+      "Access-Control-Allow-Origin": "https://gleaming-narwhal-31cfb9.netlify.app",
     },
     host: "localhost",
     proxy: "http://localhost:8000/",
